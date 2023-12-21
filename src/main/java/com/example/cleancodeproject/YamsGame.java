8bd59e8bda1b5ac1af6885e7a89d57d4e7adad94
AbdallahSlimane <slimane.abdallah75@gmail.com>
2023-12-21 08:56:00 +0100

    add functions for brelan, carre, full, yams and somme

--- src/main/java/com/example/cleancodeproject/YamsGame.java
+++ src/main/java/com/example/cleancodeproject/YamsGame.java
@@ -2,6 +2,7 @@ package com.example.cleancodeproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class YamsGame {
@@ -10,7 +11,32 @@ public class YamsGame {
     }
 
     public static int score(int[] des) {
-        return 0;
+        Arrays.sort(des);
+
+        if (isYams(des)) return 50;
+        if (isCarre(des)) return 35;
+        if (isFull(des)) return 30;
+        if (isBrelan(des)) return 28;
+        return sommeDes(des);
     }
 
+    private static boolean isBrelan(int[] des) {
+        return des[0] == des[2] || des[1] == des[3] || des[2] == des[4];
+    }
+
+    private static boolean isCarre(int[] des) {
+        return des[0] == des[3] || des[1] == des[4];
+    }
+
+    private static boolean isYams(int[] des) {
+        return des[0] == des[4];
+    }
+
+    private static boolean isFull(int[] des) {
+        return des[0] == des[1] && des[2] == des[4] || des[0] == des[2] && des[3] == des[4];
+    }
+
+    private static int sommeDes(int[] des) {
+        return Arrays.stream(des).sum();
+    }
 }
